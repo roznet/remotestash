9c326643627ec5e9d4fd6c343775ae68e1975db7
brice <briceguard-git@yahoo.com>
2020-05-30 10:28:37 +0100

    local work with properly handling content-type

--- bin/remotestash.py
+++ bin/remotestash.py
@@ -2,6 +2,7 @@
 
 from zeroconf import ServiceBrowser, Zeroconf, ServiceInfo
 import hashlib
+import mimetypes
 import urllib3
 import ssl
 import socket
@@ -12,34 +13,163 @@ import argparse
 from http.server import BaseHTTPRequestHandler
 from http.server import HTTPServer
 from urllib import parse
+import cgi
 from requests import Session, Request
 import pwd
 import json
 import sys
 from pprint import pprint
 
-def file_hash(filepath):
-    openedFile = open(filepath)
-    readFile = openedFile.read()
+class Item:
+    def __init__(self,info):
+        self.info = info.copy()
+        self.data = None
+        self.text = None
 
-    hash = hashlib.sha1(readFile.encode('utf-8'))
-    return hash.hexdigest()
 
-def content_hash(content):
-    if isinstance(content,str):
-        data = content.encode('utf-8')
-    else:
-        data = content
-    hash = hashlib.sha1(data)
-    return hash.hexdigest()
+    def __repr__(self):
+        return f'Item({self.info})'
+    
+    def from_file(info,infile):
+        '''
+        info should contains 'Content-Type' and 'file'
+        '''
+        rv = None
+        rv = Item(info)
+        rv.data = infile.read()
+        if isinstance( rv.data,str ):
+            rv.text = rv.data
+            rv.data = None
+            
+        return rv
+        
+    def from_filename(info,location=None):
+        '''
+        info should contains 'Content-Type' and 'file'
+        '''
+        filename = info['file']
+        if location:
+            filename = os.path.join( location, filename )
+        rv = None
+        if os.path.isfile( filename ):
+            with open(filename, 'rb') as readf:
+                rv = Item.from_file( info, readf )
+        else:
+            rv = Item(info)
+            
+        return rv
+
+    def from_data(data,info):
+        rv = Item(info)
+        rv.data = data
+        
+        return rv
+
+    def from_string(text,info):
+        rv = Item(info)
+        rv.text = text
+        
+        return rv
+
+    def validate_info(self):
+        if 'Content-type' not in self.info:
+            print( 'Missing Content Type' )
+
+    def encoding(self,strict=False):
+        ctype, options = cgi.parse_header( self.info['Content-type'] )
+        encoding = None
+        if 'charset' in options:
+            encoding = options['charset']
+
+        if not encoding:
+            if ctype.startswith( 'text/' ):
+                encoding = 'utf-8'
+
+        if not encoding and not strict:                      
+            if ctype.endswith( 'xml' ) or ctype.endswith('json'):
+                encoding = 'utf-8'
+
+        return encoding
+
+    def ensure_filename(self,filename=None):
+        if filename:
+            self.info[ 'file'] =  filename
+        else:
+            data = self.as_data()
+            hash = hashlib.sha1(data)
+            self.info[ 'file'] =  hash.hexdigest()
+
+    def json_dict(self):
+        return self.info
+    
+    def save_to_file(self,location=None):
+        self.ensure_filename()
+        filename = self.info['file']
+        path = filename
+        if location:
+            path = os.path.join( location, filename )
+            
+        if self.data:
+            with open( path, 'wb' ) as of:
+                of.write( self.data )
+        elif self.text:
+            with open( path, 'w' ) as of:
+                of.write( self.text )
+
+
+    def clear_file(self,location=None):
+        if 'file' in self.info:
+            path = self.info['file']
+            if location:
+                path = os.path.join( location, path )
+            if os.path.isfile(path ):
+                os.remove( path )
+            
+    def as_str(self):
+        if self.text:
+            return self.text
+
+        if self.data:
+            encoding = self.encoding()
+            if encoding:
+                return self.data.decode( encoding )
+
+        return None
+
+    def as_data(self):
+        if self.data:
+            return self.data
+        
+        if self.text:
+            encoding = self.encoding()
+            if encoding:
+                return self.text.encode( encoding )
+
+        return None
+    
+    def output(self,outfile=sys.stdout):
+        if not self.data and not self.text:
+            return
+        
+        if 'b' in outfile.mode:
+            data = self.as_data()
+            outfile.write( data )
+        else:
+            data = self.as_str()
+            outfile.write( data )
 
 class Stash:
     def __init__(self,args):
         self.args = args
+        self.verbose = args.verbose
         if 'dir' in self.args:
             self.location = self.args['dir']
         else:
             self.location = os.path.expanduser("~/.remotestash")
+            if not os.path.isdir( self.location ):
+                if self.verbose:
+                    print( f'Creating {self.location}' )
+                os.makedirs( self.location )
         self.content_file = os.path.join( self.location, 'contents.json' )
         if os.path.isfile( self.content_file ):
             with open( self.content_file ) as jf:
@@ -47,43 +177,48 @@ class Stash:
         else:
             self.contents = {'items':[]}
 
+        if self.verbose:
+            size = len(self.contents['items'])
+            print( f'Local Stash {self.location} {size} items' )
+
     def pull(self):
         if len(self.contents['items']):
-            item = self.contents['items'].pop()
-            datafile = os.path.join( self.location, item['file'] )
-            data = None
-            if os.path.isfile( datafile ):
-                with open(datafile, 'r' ) as readf:
-                    data = readf.read()
-                os.remove(datafile)
+            info = self.contents['items'].pop()
+            item = Item.from_filename(info,self.location)
+            item.clear_file(self.location)
             self.save_content()
-            return data
+            if self.verbose:
+                size = len(self.contents['items'])
+                print( f'Local Stash pull {item} [{size} items]' )
+            
+            return item
+        else:
+            if self.verbose:
+                print( f'Local Stash empty no item to pull' )
+                
 
         return None
     
     def last(self):
         if len(self.contents['items']):
-            item = self.contents['items'][-1]
-            datafile = os.path.join( self.location, item['file'] )
-            data = None
-            if os.path.isfile( datafile ):
-                with open(datafile, 'r' ) as readf:
-                    data = readf.read()
-            return data
+            item = Item.from_filename(self.contents['items'][-1],self.location)
+            return item
 
-        return ''
+        return None
 
-    def push(self,name,data):
-        item = { 'name': name, 'file': content_hash( data ) }
-
-        with open( os.path.join( self.location, item['file'] ), 'wb' if isinstance(data,bytes) else 'w' ) as of:
-            of.write( data )
-        self.contents['items'].append( item )
+    def push(self,item):
+        item.ensure_filename()
+        item.save_to_file(self.location)
+        self.contents['items'].append( item.json_dict() )
+        if self.verbose:
+            size = len(self.contents['items'])
+            print( f'Local Stash pushed {item} [{size} items]' )
         self.save_content()
 
     def save_content(self):
         with open( self.content_file, 'w' ) as jf:
             json.dump( self.contents, jf )
+
 
 class Listener:
 
@@ -337,18 +472,43 @@ class Driver :
     def get_input_file(self):
         if 'file' in self.args and self.args.file:
             if os.path.isfile( self.args.file ):
-                return open( self.args.file, 'r' )
+                return open( self.args.file, 'rb' )
             else:
                 return None
         else:
             return sys.stdin
+        
+    def get_content_type(self):
+        if self.args.content_type:
+            return self.args.content_type
+
+        if 'file' in self.args and self.args.file:
+            if os.path.isfile( self.args.file ):
+                ct = mimetypes.guess_type( self.args.file )
+                if ct[1]:
+                    ct = f'{ct[0]}; charset: {ct[1]}'
+                else:
+                    ct = ct[0]
+                return( ct )
+            
+        return 'text/plain; charset=utf-8'
+        
+    def get_output_file(self):
+        if 'output' in self.args and self.args.output:
+            if self.verbose:
+                print( f'Saving to {self.args.output}' )
+            return open( self.args.output, 'wb' )
+        else:
+            return sys.stdout
             
     def cmd_push(self):
         if self.args.local:
             inputf = self.get_input_file()
-            content = inputf.read()
+            content_type = self.get_content_type()
+            
+            item = Item.from_file( {'Content-type': content_type}, inputf )
             stash = Stash(self.args)
-            stash.push( 'nn', content )
+            stash.push( item )
         else:
             self.cmd_listen('push')
             
@@ -356,19 +516,26 @@ class Driver :
     def cmd_pull(self):
         if self.args.local:
             stash = Stash(self.args)
-            data = stash.pull()
-            print( data )
+            item = stash.pull()
+            if item:
+                item.output(self.get_output_file())
         else:
             self.cmd_listen('pull')
             
     def cmd_last(self):
         if self.args.local:
             stash = Stash(self.args)
-            data = stash.last()
-            print( data )
+            item = stash.last()
+            item.output(self.get_output_file())
         else:
             self.cmd_listen('last')
 
+    def cmd_test(self):
+        a = Item.from_string( 'hello', { 'Content-type': 'text/plain; charset=utf-8' } )
+
+        print( a.as_data() )
+        print( a.as_str() )
+        
             
 if __name__ == "__main__":
     commands = {
@@ -378,16 +545,19 @@ if __name__ == "__main__":
         'last':{'attr':'cmd_last','help':'push content to stash'},
         'pull':{'attr':'cmd_pull','help':'pull content to stash'},
         'list':{'attr':'cmd_list','help':'list stash'},
+        'test':{'attr':'cmd_test','help':'random tests'},
     }
     
     description = "\n".join( [ '  {}: {}'.format( k,v['help'] ) for (k,v) in commands.items() ] )
     
     parser = argparse.ArgumentParser( description='Remote Copy', formatter_class=argparse.RawTextHelpFormatter )
     parser.add_argument( 'command', metavar='Command', help='command to execute:\n' + description)
+    parser.add_argument( '-c', '--content-type', help='content type' )
     parser.add_argument( '-l', '--local', action='store_true', help='use local stash' )
     parser.add_argument( '-n', '--name', help='name for service' )
     parser.add_argument( '-v', '--verbose', action='store_true', help='verbose output' )
     parser.add_argument( '-p', '--port', help='port to use if not set will use a free port' )
+    parser.add_argument( '-o', '--output', help='output file if not set save to stdout' )
     parser.add_argument( 'file',    metavar='FILE', nargs='?' )
     args = parser.parse_args()
 
